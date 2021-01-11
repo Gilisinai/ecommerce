968589d2465b85ad95e5f708bc71102317f8d7db
Gili Sinai <gilisinai2016@gmail.com>
2021-01-11 16:35:51 +0200

    added favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import StripeCheckout from 'react-stripe-checkout'
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51I8PzsCpZLEVnixtd3J5UYkHWkrApqTlCFzO7SGfz4KMeCwEEvEDL4ye7IElT1NQtji9RDPuETHyh4DtOktBK7SZ00rU4fo0oo'
+
+    const onToken = token => {
+        console.log(token)
+        alert('Payment Successful')
+    }
+
+    return (
+        <StripeCheckout 
+            label='Pay Now'
+            name='eccomerce app'
+            billingAddress
+            shippingAddress
+            image='https://sendeyo.com/up/d/f3eb2117da'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton

23bf458ef160bb3e551be57c0367e41171b80939
Gili Sinai <gilisinai2016@gmail.com>
2021-01-12 19:11:02 +0200

    commit before redux thunk

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions'
 import { selectCurrentUser } from './redux/user/user.selectors'
+import { selectCollectionsForPreview } from './redux/shop/shop.selectors'
 
 class App extends React.Component {
 
@@ -20,7 +21,7 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    const { setCurrentUser } = this.props
+    const { setCurrentUser} = this.props
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth)
@@ -36,6 +37,7 @@ class App extends React.Component {
       } else {
         setCurrentUser(userAuth)
       }
+      // addCollectionAndDocuments('collections', collectionsArray.map(({title, items}) => ({title, items })))
     })
   }
 
@@ -60,7 +62,8 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
+  
 })
 
 const mapDispatchToProps = dispatch => ({
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,11 +1,18 @@
-import SHOP_DATA from './shop.data'
+
+
+import ShopActionTypes from './shop.types'
 
 const initailState = {
-    collections: SHOP_DATA
+    collections: null
 }
 
 const shopReducer = (state= initailState, action) => {
     switch(action.type){
+        case ShopActionTypes.UPDATE_COLLECTIONS:
+            return {
+                ...state,
+                collections: action.payload
+            }
         default:
             return state
     }
